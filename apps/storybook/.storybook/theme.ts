536b75bdd1b3dbab9e05cfc7821482e68b5ea4a4
Hasani Rogers <contact@deificarts.com>
2026-09-06 12:51:01 -0700

    fix

--- apps/storybook/.storybook/theme.ts
+++ apps/storybook/.storybook/theme.ts
@@ -4,6 +4,6 @@ import Logo from './logo.svg';
 export default create({
   base: 'dark',
   brandTitle: 'Kemet UI | Storybook',
-  brandUrl: 'https://kemet.dev/docs',
+  brandUrl: 'https://kemet.dev',
   brandImage: Logo
 });
